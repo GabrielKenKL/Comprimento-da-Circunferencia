024bd43a61cd8d0ec2c0e8773393f7ad6b54e159
Gabriel Ken Kudo de Lima <gabkenkudo@gmail.com>
2026-03-02 11:43:25 -0300

    Primeira versão dos códigos.

---
+++ Comprimento da circunferência.py
@@ -0,0 +1,10 @@
+print("Cálculo do comprimento da circunferência:")
+print("NOTA: pi = 3.14")
+
+print("Digite o valor do raio da circunferência:")
+raio = float(input())
+
+#Cálculo do comprimento da circunferência:
+comprimento = 3.14*2*raio
+
+print("O comprimento da circunferência é:", comprimento)
